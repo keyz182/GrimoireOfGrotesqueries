9923a1d84bed847950f95a9ffc45fde914a67dd3
Kieran David Evans <keyz182@gmail.com>
2025-04-24 13:21:18 +0100

    Cookie cutter initial commit

---
+++ 1.5/Source/GrimoireOfGrotesqueries/GrimoireOfGrotesqueriesDefOf.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+
+namespace GrimoireOfGrotesqueries;
+
+[DefOf]
+public static class GrimoireOfGrotesqueriesDefOf
+{
+    // Remember to annotate any Defs that require a DLC as needed e.g.
+    // [MayRequireBiotech]
+    // public static GeneDef YourPrefix_YourGeneDefName;
+
+    static GrimoireOfGrotesqueriesDefOf() => DefOfHelper.EnsureInitializedInCtor(typeof(GrimoireOfGrotesqueriesDefOf));
+}
---
+++ 1.5/Source/GrimoireOfGrotesqueries/GrimoireOfGrotesqueriesMod.cs
@@ -0,0 +1,34 @@
+﻿using Verse;
+using UnityEngine;
+using HarmonyLib;
+
+namespace GrimoireOfGrotesqueries;
+
+public class GrimoireOfGrotesqueriesMod : Mod
+{
+    public static Settings settings;
+
+    public GrimoireOfGrotesqueriesMod(ModContentPack content) : base(content)
+    {
+        Log.Message("Hello world from Sir Hinus' Grimoire of Grotesqueries");
+
+        // initialize settings
+        settings = GetSettings<Settings>();
+#if DEBUG
+        Harmony.DEBUG = true;
+#endif
+        Harmony harmony = new Harmony("keyz182.rimworld.GrimoireOfGrotesqueries.main");
+        harmony.PatchAll();
+    }
+
+    public override void DoSettingsWindowContents(Rect inRect)
+    {
+        base.DoSettingsWindowContents(inRect);
+        settings.DoWindowContents(inRect);
+    }
+
+    public override string SettingsCategory()
+    {
+        return "Sir Hinus' Grimoire of Grotesqueries_SettingsCategory".Translate();
+    }
+}
